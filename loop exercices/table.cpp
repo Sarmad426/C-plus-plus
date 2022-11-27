bd4fd8198ab05afb431b8dcd8a4a096e608a0269
Sarmad Rafique <sarmadrafique040@gmail.com>
2022-11-27 23:36:03 +0500

    Table of user input number in C++

---
+++ loop exercices/table.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int table;
+	cout<<"Enter Number for the Table: "<<endl;
+	cin>>table;
+	for(int num=1;num<=10;num++){
+		cout<<num*table<<endl;
+	}
+}
