aa4033070e6bf2f89f3f657895c83df41abffa07
Sarmad rafique <sarmadrafique040@gmail.com>
2022-12-19 19:09:31 +0500

    A Program That Converts an Uppercase Character to Lowercase and Lowercase Character To Uppercase:

---
+++ exercises/lowercase to uppercase.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char character;
+	cout<<"Enter Your Name: "<<endl;
+	cin>>character;
+	int convert_int=int(character);
+	if(character>='A' && character<='Z'){
+	convert_int+=32;	
+	cout<<char(convert_int)<<" in Lowercase: ";
+	}
+	else if (character>='a' && character<='z'){
+		convert_int-=32;
+		cout<<char(convert_int)<<" in Uppercase";
+	}
+	else{
+		cout<<"Invalid input: "<<character<< " is not an alphabetic character: ";
+	}
+}
