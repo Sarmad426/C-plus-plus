3810cdef65eccfd0010470fbfdc5a0a7d6081952
Sarmad rafique <sarmadrafique040@gmail.com>
2023-01-09 15:02:56 +0500

    Updated the Code:

--- PF finals/shapes/left triangle.cpp
+++ PF finals/shapes/left triangle.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 	// Left Triangle 
-for(int i=0;i<5;i++){
+for(int i=0;i<6;i++){
 	for(int j=0;j<=i;j++){
 		cout<<"*";
 	}
--- PF finals/shapes/shape2.cpp
+++ PF finals/shapes/shape2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 	// Reverse Triangle
-	for(int i=5;i>0;i--){
+	for(int i=7;i>0;i--){
 		for(int j=0;j<i;j++){
 			cout<<"*";
 		}
